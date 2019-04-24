768eae0b6e011beb32bd32619f6dccc54a5e672e
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-24 11:01:55 -0700

    Add new Class

---
+++ ConsoleApp1/ConsoleApp1/MathAndComparison.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class MathAndComparison
+    {
+        static void Main(string[] args)
+        {
+            int total = 5 + 10;
+            int othertotal = 12 + 22;
+            int combined = total + othertotal;
+            Console.WriteLine(combined);
+            // Console.WriteLine("Five Plus Ten" + total);
+            Console.ReadLine();
+        }
+    }
+}
