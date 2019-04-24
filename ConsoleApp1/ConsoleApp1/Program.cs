2ef8985762580c019eeded0bfe90b9a875218eab
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-24 10:13:46 -0700

    delete files

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -24,4 +24,38 @@ namespace ConsoleApp1
 
         }
     }
+    class DailyReport
+    {
+        static public void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            String Course = Console.ReadLine();
+            int chnage = Convert.ToInt32(Course);
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
+            bool answer = true;
+            string ans = Convert.ToString(answer);
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            String Experience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string Otherexperience = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            // int hours = Console.ReadLine();
+            //String changeHours = Convert.ToString(hours);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        }
+    }
+    class Class1
+    {
+        static public void Main()
+        {
+            Console.WriteLine("Write Your favorite number?");
+            string favnumber = Console.ReadLine();
+            int favnum = Convert.ToInt32(favnumber);
+            int total = favnum + 5;
+            Console.WriteLine("Your favorite number plus 5 is :" + total);
+            Console.ReadLine();
+        }
+    }
 }
