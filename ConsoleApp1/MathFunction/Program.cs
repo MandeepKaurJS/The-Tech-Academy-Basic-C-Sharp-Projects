e9f90b1ab127bac763b43c0b06d527acfdec215f
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-24 13:16:48 -0700

    Add new project

---
+++ ConsoleApp1/MathFunction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
