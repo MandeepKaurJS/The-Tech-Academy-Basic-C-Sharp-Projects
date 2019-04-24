24a2d91f03009c774a0ae2aabca9ea774a4a142d
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-24 10:01:39 -0700

    update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,12 +6,12 @@ namespace ConsoleApp1
     {
         static void Main()
         {
+
             Console.WriteLine("Welcome to Acme Accounting Systems");
             Console.WriteLine("Remember, we’re “accounting” on you!");
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
             Console.WriteLine("Welcome to Acme Accounting System " + name + "");
-            Console.Read();
             Console.WriteLine("How old are you?");
             string age = Console.ReadLine();
 
@@ -20,6 +20,7 @@ namespace ConsoleApp1
            // Console.WriteLine("You are " + ageInCenturies + " centuries old.");
 
             //Prints "You are .31 centuries old"
+
 
         }
     }
