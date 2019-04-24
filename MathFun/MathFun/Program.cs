6608ed931fa149f9038f9891d14ac4429ee52a49
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-24 12:55:48 -0700

    Add some statements

--- MathFun/MathFun/Program.cs
+++ MathFun/MathFun/Program.cs
@@ -6,7 +6,20 @@ namespace MathFun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.Write("Enter an integer");
+            long Number =Convert.ToInt64(Console.ReadLine());
+            long multiply = Number * 50;
+            Console.WriteLine("Multiply with 50 : \n" + multiply);
+
+            /*Second question */
+
+            Console.WriteLine("Enter An Integer");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int Add = num + 25;
+            Console.WriteLine("Number After Addition: " + Add);
+
+            
         }
     }
 }
