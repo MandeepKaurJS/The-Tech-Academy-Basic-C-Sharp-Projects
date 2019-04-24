c11cf8d1c80fc03eca655e29439111e29e7475da
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-24 16:43:00 -0700

    Add condition statement

--- CarInsuranceProgram/Program.cs
+++ CarInsuranceProgram/Program.cs
@@ -13,12 +13,21 @@ namespace CarInsuranceProgram
             Console.WriteLine("What is your age?");
             int age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Have you ever had a DUI? Give answer into true or false ");
-            string Dui=Convert.ToString(Console.ReadLine());
+            string Dui = Console.ReadLine();
+            Convert.ToBoolean(Dui);
             Console.WriteLine("How many speeding tickets do you have?");
             int tickets = Convert.ToInt32(Console.ReadLine());
-            bool qualified= (age <= 14 && Dui = false && tickets <= 3);
-            Console.WriteLine("Qualified?"+qualified);
-            
+            //int qualified= (age <= 14 && Dui ='false' && tickets <= 3);
+            //Console.WriteLine("Qualified?"+qualified);
+            if(age >= 14 && tickets>=3)
+            {
+                Console.WriteLine("Qualified");
+                Console.ReadLine();
+
+            }else
+            {
+                Console.WriteLine("Not Qualified!");
+            }
             
 
         }
