79c11d10e8282509efc255f7b5b2e76aef9afd12
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-24 14:18:43 -0700

    Add new variables and statements

---
+++ ConsoleApp1/MATH_AND_COMPARISON_OPERATORS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MATH_AND_COMPARISON_OPERATORS
+{
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate?");
+            int rate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            int weeks = Convert.ToInt32(Console.ReadLine());
+            /****Person 2***/
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate?");
+            int rate2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            int weeks2 = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
