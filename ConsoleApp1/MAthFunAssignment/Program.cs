5703a33d95f4f91f97d48cd4be73b731a6b0d326
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-24 14:05:14 -0700

    Update

--- ConsoleApp1/MAthFunAssignment/Program.cs
+++ ConsoleApp1/MAthFunAssignment/Program.cs
@@ -7,39 +7,38 @@ namespace MAthFunAssignment
         static void Main(string[] args)
         {
             /**First Question ***/
-            Console.WriteLine("Enter an Integer");
+            Console.WriteLine("Enter an Integer to multiply it with 50");
             long Number = Convert.ToInt64(Console.ReadLine());
             long Multiply = Number * 50;
-            Console.WriteLine("Multiply with 50: " + Multiply);
+            Console.WriteLine("After Multiply with 50 number is: " + Multiply);
 
             /***Second Question ***/
 
-            Console.WriteLine("Enter an Integer");
+            Console.WriteLine("Enter an Integer to add it into 25");
             int num = Convert.ToInt32(Console.ReadLine());
             int Add = num + 25;
             Console.WriteLine("Number after adding into 25: " + Add);
 
             /***Third Question ***/
 
-            Console.WriteLine("Enter any number ");
+            Console.WriteLine("Enter any number to divide it by 12.5 number ");
             double number = Convert.ToDouble(Console.ReadLine());
             double quotient = number / 12.5;
             Console.WriteLine(" We can Divide any number with 12.5: " + quotient);
 
             /***Forth Question ***/
 
-            Console.WriteLine("Enter any number :");
+            Console.WriteLine("Enter any number to check is number greater than or less than 50 :");
             int Num = Convert.ToInt32(Console.ReadLine());
             bool isnumGreater = Num > 50;
             Console.WriteLine(isnumGreater);
-            Console.ReadLine();
 
             /***Fifth Question ***/
 
-            Console.WriteLine("Enter an Integer");
+            Console.WriteLine("Enter an Integer to divide it by 7");
             int Divide = Convert.ToInt32(Console.ReadLine());
             int divided = Divide % 7;
-            Console.WriteLine("Remainder is :" + divided);
+            Console.WriteLine("after divide by 7 remainder is: " + divided);
             Console.ReadLine();
         }
     }
