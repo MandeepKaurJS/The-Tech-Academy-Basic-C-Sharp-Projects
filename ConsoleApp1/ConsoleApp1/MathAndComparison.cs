3cf3f420f9169aaab80165fec62644a4cdd5bcab
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-24 11:53:49 -0700

    add basics Math to class

--- ConsoleApp1/ConsoleApp1/MathAndComparison.cs
+++ ConsoleApp1/ConsoleApp1/MathAndComparison.cs
@@ -8,11 +8,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.Write("Add two variables");
             int total = 5 + 10;
             int othertotal = 12 + 22;
             int combined = total + othertotal;
             Console.WriteLine(combined);
             // Console.WriteLine("Five Plus Ten" + total);
+            Console.ReadLine();
+            Console.Write("Substract two variables");
+            int difference = 10 - 5;
+            Console.WriteLine("Ten Minus five"+difference);
+            Console.ReadLine();
+            Console.Write("Multiply two variables");
+            int product = 10 * 5;
+            Console.WriteLine("" + product);
+            Console.ReadLine();
+            Console.Write("Divide two variables");
+            int quotient = 100 / 5;
+            Console.WriteLine("Divide by 5" + quotient);
+            Console.ReadLine();
+            Console.Write("Divide decimal two variables");
+            double quotient1 = 100.0 / 17.5;
+            Console.WriteLine("Divide by 17.5" + quotient1);
+            Console.ReadLine();
+            Console.Write("Divide  two variables which gives remainder");
+            int remainder = 10 % 2;
+            Console.WriteLine("Divide by 2" + remainder);
             Console.ReadLine();
         }
     }
