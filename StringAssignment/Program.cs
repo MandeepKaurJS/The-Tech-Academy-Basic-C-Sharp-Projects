5f9a7a05b476b26523f3c6e3f77a163d685179e0
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-25 14:13:48 -0700

    Adding something make it runnable

--- StringAssignment/Program.cs
+++ StringAssignment/Program.cs
@@ -10,7 +10,40 @@ namespace StringAssignment
     {
         static void Main(string[] args)
         {
-
+            string name = "Mandeep Kaur.";
+            Console.WriteLine("Hi this is " + name);
+            string work = " taking classes from Tech Academy.";
+            Console.WriteLine("I Am " + work);
+            string hours = "spend 8 hours per day in weekdays.";
+            Console.WriteLine("I " + hours);
+            Console.WriteLine("To see everything into one line press enter");
+            Console.ReadLine();
+            String join = "I'm "+name + "I am "+ work +"I "+ hours;
+            Console.WriteLine("MY Current Status \n" + join);
+            Console.ReadLine();
+            join=join.ToUpper();
+            Console.WriteLine("Now I'm in Upper Case \n"+join);
+            Console.Read();
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Taking ");
+            sb.Append("classes ");
+            sb.Append("from ");
+            sb.Append("Tech ");
+            sb.Append("Acedemy ");
+            sb.Append("make ");
+            sb.Append("be ");
+            sb.Append("able ");
+            sb.Append("to ");
+            sb.Append("take ");
+            sb.Append("a ");
+            sb.Append("step ");
+            sb.Append("into ");
+            sb.Append("IT ");
+            sb.Append("world.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+            Console.Read();
         }
+
     }
 }
