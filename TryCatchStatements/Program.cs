2ad816d599b324fb19dc293fdddd0fa59982f7fd
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-26 15:07:30 -0700

    Add Try and Catch block for mathematics purpose

--- TryCatchStatements/Program.cs
+++ TryCatchStatements/Program.cs
@@ -10,6 +10,35 @@ namespace TryCatchStatements
     {
         static void Main(string[] args)
         {
+            try
+            {
+
+                int[] number = { 2, 3, 4, 5, 6, 7, 8 };
+                Console.WriteLine("Pick any number");
+                float num = Convert.ToInt32(Console.ReadLine());
+                for (var i = 0; i < number.Length; i++)
+                {
+                    float Divide = number[i] / num;
+                    Console.WriteLine("After Dividing number is : "+Divide);
+                    Console.ReadLine();
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Choose Another num, We Can't divide any number by Zero" + ex);
+            }
+            catch (System.FormatException e)
+            {
+                Console.WriteLine("Integer is not Divide by string or chararcter" + e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
