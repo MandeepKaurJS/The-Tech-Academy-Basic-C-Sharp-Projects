635c01553505e7067a7e5bd7ee6566c05f3e5d18
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-25 23:22:36 -0700

    Add some functionality

--- ArrayAndListItration/Program.cs
+++ ArrayAndListItration/Program.cs
@@ -11,17 +11,61 @@ namespace ArrayAndListItration
     {
         static void Main(string[] args)
         {
-            string[] value= new string[10];
+            string[] value = new string[10];
             int i = 0;
-            for(i = 0; i <10; i++)
+            for (i = 0; i < 10; i++)
             {
                 Console.WriteLine("Enter some text");
                 value[i] = Console.ReadLine();
-                Console.WriteLine(value[i]);
-                
+            }
+            Console.WriteLine("The values you've entered are :");
+            Console.WriteLine(String.Join(",", value));
+            Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("looping...");
+
+                if (Console.KeyAvailable)
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();
+            Console.Write(" iterating the loop is a “<=” operator.");
+            int count = 4;
+            for (int j = 1; j <= count; j++)
+            {
+                Console.WriteLine("Current value of i is - " + j);
+            }
+            Console.WriteLine(" iterating the loop is a “<” operator.");
+            int count1 = 4;
+            for (int k = 1; k < count1; k++)
+            {
+                Console.WriteLine("Current value of i is - " + k);
+            }
+            Console.ReadLine();
+            List<string> list = new List<string>()
+            { "Mandeep","kaur","Sidhu","Dulay","Done"};
+            for (int str = 0; str < list.Count; str++)
+            {
+                Console.WriteLine("Search any string");
+                string search = Convert.ToString(Console.ReadLine());
+
+                if (search==list[str])
+                {
+                    Console.WriteLine("Search Result is at index ");
+                    int index = str;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("String is not in list");
+                    Console.ReadLine();
+                    break;
+                }
             }
 
-            Console.ReadLine();
+
         }
     }
 }
