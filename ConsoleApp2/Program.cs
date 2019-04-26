80f2a90b292cb0b7dc0cb799f084e4ceb3ae468a
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-25 18:08:48 -0700

    Add new project

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAndListItration
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some text");
+            string enter = Console.ReadLine();
+            string[] value= new string[100];
+            for(var i = 0; i <100; i++)
+            {
+                value[i] = enter;
+            }
+        }
+    }
+}
