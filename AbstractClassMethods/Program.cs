edc743d0018956fc2b1a35fa4768fec307c840ce
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-30 12:26:35 -0700

    Add new project
    Added new project into repository which shows working of abstract with abstract methods.

---
+++ AbstractClassMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person name = new Employee();
+            name.FirstName = "Sample";
+            name.LastName = "Student";
+            name.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}
