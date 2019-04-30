1c50b940530c792692c72887d4c221775226dd5d
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-29 17:13:23 -0700

    Add new porject and update cardGame

--- CardsGame/Program.cs
+++ CardsGame/Program.cs
@@ -11,28 +11,46 @@ namespace CardsGame
         static void Main(string[] args)
         {
             Deck deck = new Deck();
-            deck = Shuffle(deck);
+            int shuffled = 0;
+            deck = Shuffle(deck,out shuffled,3);
             foreach(Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("time shuffle {0}:", shuffled);
             Console.ReadLine();
 
         }
-        public static Deck Shuffle(Deck deck)
+        public static Deck Shuffle(Deck deck,out int shuffled,int times=1)
         {
-            List<Card> templist = new List<Card>();
-            Random random = new Random();
-            while (deck.Cards.Count > 0)
+            shuffled = 0;
+            for(int i = 0; i < times; i++)
             {
-                int randomIndex = random.Next(0, deck.Cards.Count);
-                templist.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex);
+                shuffled++;
+                List<Card> templist = new List<Card>();
+                Random random = new Random();
+                while (deck.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, deck.Cards.Count);
+                    templist.Add(deck.Cards[randomIndex]);
+                    deck.Cards.RemoveAt(randomIndex);
+                }
+                deck.Cards = templist;
+                
             }
-            deck.Cards = templist;
             return deck;
-            
+
+
+
         }
+        //public static Deck Shuffle(Deck deck,int times)
+        //{
+        //    for (int i = 0; i < times; i++)
+        //    {
+        //        deck = Shuffle(deck);
+        //    }
+        //    return deck;
+        //}
     }
 }
