42c1297093266a530a05a1e7a9afbcf33d3477c8
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-29 18:40:15 -0700

    Methods for optional number

---
+++ OptionalMethodsinClass/OptionalParameter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalMethodsinClass
+{
+    class OptionalParameter
+    {
+        public void Additon(int num, int optionalint=10)
+        {
+            int result = num * 2;
+            Console.WriteLine("First parameter after Multiplying by 2: \n " + result);
+            if (optionalint == optionalint)
+            {
+                result = num * 2 + optionalint;
+                Console.WriteLine("Number after Adding optional parameter into " +
+                    "first parameter " + result);
+            }
+            else
+            {
+                Console.Write("No need to type another varible");
+            }
+        }
+    }
+}
---
+++ OptionalMethodsinClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalMethodsinClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OptionalParameter number = new OptionalParameter();
+            Console.WriteLine("Enter First number");
+            int result = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n No need to enter second number: ");
+            int result2 = 0;
+            Console.ReadLine();
+            number.Additon(result);
+            
+            number.Additon(result, result2);
+            number.Additon(result, result2);
+            number.Additon(result);
+            Console.ReadLine();
+        }
+    }
+}
