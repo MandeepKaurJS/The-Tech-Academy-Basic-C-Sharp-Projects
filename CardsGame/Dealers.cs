4a03ceb2716a415531a0b45fef1d9f1afdc01304
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-30 11:58:53 -0700

    Added two more classes into it

---
+++ CardsGame/Dealers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsGame
+{
+    public class Dealers
+    {
+        public string Name { get; set; }
+        public Deck Deck1 { get; set; }
+        public int balacne { get; set; }
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck1.Cards.First());
+            Console.WriteLine(Deck1.Cards.First().ToString());
+            Deck1.Cards.RemoveAt(0);
+        }
+    }
+}
---
+++ CardsGame/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsGame
+{
+   public class Player
+    {
+        public List<Card> hand { get; set; }
+        public int Balance { get; set; }
+        public string Name { get; set; }
+        public bool isActivePlaying { get; set; }
+    }
+}
