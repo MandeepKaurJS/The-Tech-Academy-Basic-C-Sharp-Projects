1020677e751fdffab70519c19a2d0238ac74cbb1
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-29 18:12:22 -0700

    Add three methods into methodCall class

---
+++ MethodsAndCalling/MethodsCall.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndCalling
+{
+    class MethodsCall
+    {
+        public void Addition(int add)
+        {
+            try
+            {
+                int result = add / 2;
+                Console.WriteLine("Divide number by 2: " + result);
+
+            }catch(DivideByZeroException e)
+            {
+                Console.WriteLine("we cannot divide any number with 0");
+            }
+            
+            
+        }
+        public void Addition(double mul)
+        {
+            double result = mul * mul;
+            Console.WriteLine("Multiplication of decimal Number is : " + result);
+        }
+        public void Addition(string show)
+        {
+            try
+            {
+                int showresult = Int32.Parse(show+show);
+                Console.WriteLine("Add string by itself:" +showresult);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Unable to parse '{show}'");
+            }
+
+        }
+    }
+}
---
+++ MethodsAndCalling/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndCalling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodsCall callfun = new MethodsCall();
+            callfun.Addition(2);
+            callfun.Addition(2.3);
+            callfun.Addition("2");
+            Console.ReadLine();
+        }
+    }
+}
