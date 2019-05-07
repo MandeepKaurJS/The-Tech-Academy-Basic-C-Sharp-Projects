30701ad5eaa7c095743b9513d1d9c52a83ce667c
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-05-07 16:37:25 -0700

    Added Chain Constructors

--- AdditionalFeaturesofCSharp/Program.cs
+++ AdditionalFeaturesofCSharp/Program.cs
@@ -8,28 +8,48 @@ namespace AdditionalFeaturesofCSharp
 {
     class Program
     {
-        public Program(string name,int age)
+        string name;
+        int Age;
+        int Wage;
+        public Program(string Name,int age)
         {
+            name = Name;
+            Age = age;
             
         }
-        public Program() : this("Mandeep",26)
+        public Program(int wage) 
         {
+            Wage = wage;
 
         }
+        public void showResults()
+        {
+            Console.WriteLine("Constructors with 2 parameters: "+name+"\n"+Age);
+            Console.ReadLine();
+           
+        }
+        public void showOneParameter()
+        {
+            Console.WriteLine("Constructors with 1 parameter: " + Wage);
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
             const string Name = "Mandeep Kaur";
             var age = 25;
-            Console.WriteLine("Constant string and var Age" + Name + " " + age);
+            Console.WriteLine("Constant string and var Age: " + Name + "\n " + age);
             Console.ReadLine();
+            /*** if I pass Const Name value to Constructor it will remain the same not changable any more ****/
+            var PassCons = new Program(Name, age);
+            PassCons.showResults();
             /**** This is Constructor with two parameter***/
-            Program cons = new Program("Varinder", 26);
-            Console.WriteLine("Constructors with 2 parameters"+cons);
-            Console.ReadLine();
+            var cons = new Program("Mandeep",26);
+            cons.showResults();
             /****Constructor with no parameters ***/
-            Program cons2 = new Program();
-            Console.WriteLine("Constructors with no parameters"+cons2);
-            Console.ReadLine();
+            var cons2 = new Program(wage:2000);
+            cons2.showOneParameter();
+           
 
 
         }
